6431ef0ec770f02dbd6fe176fb86bc5dc15d2d9e
pedramjlo <pedram_sif@tuta.io>
2024-05-28 11:47:18 +0330

    added all dirs in the project

--- javascript/main.js
+++ javascript/main.js
@@ -1,16 +1,13 @@
 function loadPage(pageName, targetElement) {
     // Update the header image
     document.getElementById('header-image').src = `../images/${pageName}-header.webp`;
-
-    
     // Load the page content
     fetch(`${pageName}.html`)
         .then(response => response.text())
         .then(data => {
             document.getElementById('content').innerHTML = data;
         });
-
-    // Handle click event
+        
     var links = document.querySelectorAll('#navbar ul li a');
     for (var i = 0; i < links.length; i++) {
         links[i].classList.remove('active');
@@ -20,8 +17,13 @@ function loadPage(pageName, targetElement) {
     }
 }
 
-
 window.onload = function() {
+    // Preload Google Font
+    var link = document.createElement('link');
+    link.rel = 'stylesheet';
+    link.href = 'https://fonts.googleapis.com/css2?family=Lalezar&display=swap';
+    document.head.appendChild(link);
+
     var links = document.querySelectorAll('#navbar ul li a');
     for (var i = 0; i < links.length; i++) {
         links[i].addEventListener('click', function(event) {
@@ -33,3 +35,4 @@ window.onload = function() {
     var pizzaLink = document.querySelector('#navbar ul li a[data-page="pizza"]');
     loadPage('pizza', pizzaLink);
 }
+
